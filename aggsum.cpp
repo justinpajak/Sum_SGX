c1a2cc19a4cf2d6174bdd6e304e609362d55afb4
justinpajak <jpajak@nd.edu>
2021-08-09 11:25:02 -0400

    added threaded read and decrypt, large critical section. needs fix

--- aggsum.cpp
+++ aggsum.cpp
@@ -8,51 +8,66 @@
 #include <openssl/evp.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 using std::vector;
 
-double agg_sum(vector<float> nums);
+double agg_sum();
 
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
             unsigned char *iv, unsigned char *plaintext);
 
-void readAndDecrypt(vector<float>& nums);
+void readAndDecrypt();
+
+size_t NTHREADS = 1;
+size_t n = 1000000;
+vector<float> nums;
+int enc_data;
+
+// Lock
+pthread_mutex_t lock;
 
 int main(int argc, char *argv[]) {
 	
-	int n = 1000000;
-	auto start = std::chrono::high_resolution_clock::now();
-	vector<float> nums(n);
+	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			switch(argv[i][1]) {
+				case 'n':
+					n = atoi(argv[++i]);
+					break;
+				case 't':
+					NTHREADS = atoi(argv[++i]);
+					break;
+				default:
+					return EXIT_FAILURE;
+			}
+		} else {
+			return EXIT_FAILURE;
+		}
+	}
+	fprintf(stdout, "n: %lu\n", n);
+	fprintf(stdout, "t: %lu\n", NTHREADS);
+	pthread_mutex_init(&lock, NULL);
+
+	auto start_tot = std::chrono::high_resolution_clock::now();
 	
 	/* Read in data from d_enc.txt, decrypt and put in nums vector */
-	readAndDecrypt(nums);
+	enc_data = open("enc.txt", O_RDONLY);
+	readAndDecrypt();
 
 	/* Compute sum */
-	std::cout << std::fixed << "Sum: " << agg_sum(nums) << std::endl;
+	double sum = agg_sum();
+	std::cout << std::fixed << "Sum: " << sum << std::endl;
+
+	/*for (size_t i = 0; i < nums.size(); i++) {
+		std::cout << nums[i] << std::endl;
+	}*/
 
 	/* Compute time */
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-	std::cout << "n = " << n << ". Time: " << duration.count() / float(1000000) << " secs." << std::endl;
+	auto stop_tot = std::chrono::high_resolution_clock::now();
+	auto duration_tot = std::chrono::duration_cast<std::chrono::microseconds>(stop_tot - start_tot);
+	std::cout << "time: " << duration_tot.count() << std::endl;
 	return 0;
-}
-
-void readAndDecrypt(vector<float>& nums) {
-	int enc_data = open("enc.txt", O_RDONLY);
-	if (!enc_data) {
-		fprintf(stderr, "Unable to open file: %s\n", strerror(errno));
-		return;
-	}
-	unsigned char ciphertext[128];
-	unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
-    unsigned char *iv = (unsigned char *)"0123456789012345";
-	while(read(enc_data, (char*)ciphertext, 128)) {
-		unsigned char plaintext[128];
-		int plaintext_len = decrypt(ciphertext, 32, key, iv, plaintext);
-		plaintext[plaintext_len] = '\0';
-		nums.push_back(atof((const char*)plaintext));
-	}
-
 }
 
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
@@ -80,10 +95,81 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
     return plaintext_len;
 }
 
-double agg_sum(vector<float> nums) {
-	double sum;
-	for (int i = 0; i < nums.size(); i++) {
-		sum += nums[i];
+typedef struct {
+	size_t start;
+	size_t stop;
+} SumArgs;
+
+void *thread_read(void * args) {
+	SumArgs *a = (SumArgs*)args;
+
+	unsigned char ciphertext[128];
+	unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+	pthread_mutex_lock(&lock);
+	lseek(enc_data, a->start * 128, SEEK_SET);
+	while(read(enc_data, (char*)ciphertext, 128) && a->start <= a->stop) {
+		unsigned char plaintext[128];
+		int plaintext_len = decrypt(ciphertext, 32, key, iv, plaintext);
+		plaintext[plaintext_len] = '\0';
+		nums.push_back(atof((const char*)plaintext));
+		a->start++;
 	}
-	return sum;
+	pthread_mutex_unlock(&lock);
+	return NULL;
+}
+
+void readAndDecrypt() {
+	pthread_t threads[NTHREADS];
+	SumArgs args[NTHREADS];
+	
+	int e = n / NTHREADS;
+	int last = e + n % NTHREADS;
+
+	for (size_t i = 0; i < NTHREADS; i++) {
+		args[i].start = i * e;
+		args[i].stop = (i * e) + last - 1;
+		pthread_create(&threads[i], NULL, thread_read, (void*)&args[i]);
+	}
+
+	int *result;
+	for (size_t i = 0; i < NTHREADS; i++) {
+		pthread_join(threads[i], (void**)&result);
+	}
+}
+
+void *thread_sum(void *arg) {
+	SumArgs *a = (SumArgs*)arg;
+	double local_sum = 0;
+
+	for (size_t i = a->start; i <= a->stop; i++) {
+		local_sum += nums[i];
+	}
+	double *result = (double*)malloc(sizeof(double));
+	*result = local_sum;
+	return (void*)result;
+}
+
+double agg_sum() {
+	pthread_t threads[NTHREADS];
+	SumArgs args[NTHREADS];
+
+	int e = n / NTHREADS;
+	int last = e + n % NTHREADS;
+
+	for (size_t i = 0; i < NTHREADS; i++) {
+		args[i].start = i * e;
+		args[i].stop = (i * e) + last - 1;
+		pthread_create(&threads[i], NULL, thread_sum, (void*)&args[i]);
+	}
+
+	double sum_total = 0;
+	double *t_sum;
+	for (size_t i = 0; i < NTHREADS; i++) {
+		pthread_join(threads[i], (void**)&t_sum);
+		sum_total += *t_sum;
+		free(t_sum);
+	}
+
+	return sum_total;
 }
